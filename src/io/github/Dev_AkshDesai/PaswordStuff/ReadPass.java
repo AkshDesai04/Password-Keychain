ddbe60a9005c59ba1def3c81e2591c9a0eecda31
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-10-28 10:26:11 +0530

    Password Read success

--- src/io/github/Dev_AkshDesai/PaswordStuff/ReadPass.java
+++ src/io/github/Dev_AkshDesai/PaswordStuff/ReadPass.java
@@ -3,7 +3,9 @@ package io.github.Dev_AkshDesai.PaswordStuff;
 import io.github.Dev_AkshDesai.res.ReturnString;
 
 import javax.xml.bind.SchemaOutputResolver;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import static io.github.Dev_AkshDesai.FileFunctions.FileStringReader.ReadFileData;
 import static io.github.Dev_AkshDesai.res.ReturnString.Return;
@@ -47,9 +49,28 @@ public class ReadPass {
 
 
 
+		String FILE = "C:\\Users\\" +
+						new com.sun.security.
+								auth.module.
+								NTSystem().
+								getName() +
+						"\\Documents\\Password-Keychain\\" +
+						"ProjectFile.pass";
+		File file = new File(FILE);
 
 
 		String output = "";
+		Scanner Reader = new Scanner(file);
+
+		while(!output.equals("U_NAME:" + U_NAME))
+			output = Reader.nextLine();
+		output = Reader.nextLine();
+		output = Reader.nextLine();
+
+		output = output.substring(9);
+
+
+		System.out.println("output:\"" + output + "\"");
 
 
 
